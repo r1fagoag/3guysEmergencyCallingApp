c625ac4903099ae3d9e617142c99dac9dc12dfdb
Ricardo Fagoaga <r1fagoag@gmail.com>
2018-12-02 21:27:21 -0800

    Made a test login page to test logout, logout complete, ui of main page complete, starting work on adding map main content and functionality to menu options.

--- app/src/main/java/com/a3guys/emergencycallingapp/MainActivity.java
+++ app/src/main/java/com/a3guys/emergencycallingapp/MainActivity.java
@@ -1,22 +1,68 @@
 package com.a3guys.emergencycallingapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PREFERENCE_ID = "UserData";
+    public static final String Name = "Name";
+    public static final String Email = "Email";
+    public static final String SignedIn = "SignedIn";
+
+    SharedPreferences sharedPreferences;
+
+    EditText editTextName;
+    EditText editTextEmail;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences(PREFERENCE_ID, MODE_PRIVATE);
+
+        editTextName = (EditText) findViewById(R.id.login_name);
+        editTextEmail = (EditText) findViewById(R.id.login_email);
+
+        editTextName.setText(sharedPreferences.getString(Name, "Your Name"));
+        editTextEmail.setText(sharedPreferences.getString(Email, "Your Email"));
+
+
+
+
+        if(sharedPreferences.getBoolean(SignedIn, false))
+        {
+            Intent intent = new Intent(this, MainPageActivity.class);
+            startActivity(intent);
+        }
+
     }
 
-    public void changeActivity(View view) {
+    public void signIn(View view) {
 
-        Intent intent = new Intent(this, MainPageActivity.class);
-        startActivity(intent);
+        String uName = editTextName.getText().toString();
+        String uEmail = editTextEmail.getText().toString();
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(Name, uName);
+        editor.putString(Email, uEmail);
+        editor.putBoolean(SignedIn, true);
+
+        editor.commit();
+
+        if(sharedPreferences.getBoolean(SignedIn, false))
+        {
+            Intent intent = new Intent(this, MainPageActivity.class);
+            startActivity(intent);
+        }
 
     }
 }
--- app/src/main/java/com/a3guys/emergencycallingapp/MainPageActivity.java
+++ app/src/main/java/com/a3guys/emergencycallingapp/MainPageActivity.java
@@ -1,5 +1,7 @@
 package com.a3guys.emergencycallingapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +14,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainPageActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String PREFERENCE_ID = "UserData";
+    public static final String Name = "Name";
+    public static final String Email = "Email";
+    public static final String SignedIn = "SignedIn";
+
+
+    SharedPreferences sharedPreferences;
+
+    TextView textViewName;
+    TextView textViewEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +37,11 @@ public class MainPageActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.myloc);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with find my location and other actions", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -40,6 +54,19 @@ public class MainPageActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        sharedPreferences = getSharedPreferences(PREFERENCE_ID, MODE_PRIVATE);
+
+        View headerView = navigationView.getHeaderView(0);
+
+        textViewName = (TextView) headerView.findViewById(R.id.userName);
+        textViewEmail = (TextView) headerView.findViewById(R.id.userEmail);
+
+        textViewName.setText(sharedPreferences.getString(Name, "Your Name"));
+        textViewEmail.setText(sharedPreferences.getString(Email, "Your Email"));
+
+
+
     }
 
     @Override
@@ -74,23 +101,54 @@ public class MainPageActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_lost) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_accident) {
 
-        } else if (id == R.id.nav_send) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_naturaldisaster) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_drunk) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_accountedit) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+
+        } else if (id == R.id.nav_logout) {
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putBoolean(SignedIn, false);
+
+            editor.commit();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+
 
         }
 
