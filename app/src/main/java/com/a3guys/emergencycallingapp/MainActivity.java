80562f9690a98f28b96f7b2a9112fbc4967117cc
Randy Yin <randyyin@randys-mbp.home>
2018-12-04 22:51:42 -0800

    adding files

--- app/src/main/java/com/a3guys/emergencycallingapp/MainActivity.java
+++ app/src/main/java/com/a3guys/emergencycallingapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.a3guys.emergencycallingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mValueField;
+    private Button mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mValueField = (EditText) findViewById(R.id.valueField);
+        mAddButton = (Button) findViewById(R.id.addButton);
+
+
+
+
     }
 }
